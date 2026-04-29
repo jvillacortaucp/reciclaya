32f3706e638e7e1217c82bb3d46c7b64f5dfaec8
Eddu Escobedo <eescobedol@ucp.edu.pe>
2026-04-29 17:17:38 -0500

    implementacion2 de chatbot

---
+++ frontend/src/app/features/assistant-chat/infrastructure/assistant-chat.http.service.ts
@@ -0,0 +1,50 @@
+import { Injectable, inject } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { ProductSuggestion } from '../models/assistant-chat.model';
+
+export interface N8nChatResponse {
+  status: string;
+  data?: {
+    residue?: string;
+    replyText?: string;
+    suggestions?: readonly ProductSuggestion[];
+  };
+}
+
+export interface AssistantChatResponse {
+  replyText?: string;
+  suggestions?: readonly ProductSuggestion[];
+}
+
+@Injectable()
+export class AssistantChatHttpService {
+  private readonly http = inject(HttpClient);
+  private readonly webhookUrl = 'https://n8n-production-7f55.up.railway.app/webhook-test/recicla-ia'
+  //https://n8n-production-7f55.up.railway.app/webhook/recicla-ia';
+  //url test n8n https://n8n-production-7f55.up.railway.app/webhook-test/recicla-ia';
+
+  sendMessage(sessionId: string, residueInput: string): Observable<AssistantChatResponse> {
+    const headers = new HttpHeaders({
+      'x-session-id': sessionId
+    });
+
+    return this.http.post<any>(this.webhookUrl, { residuo: residueInput }, { headers }).pipe(
+      map((res) => {
+        // Fallback por si en n8n envían directamente la salida del agente LangChain
+        if (Array.isArray(res) && res.length > 0 && res[0].output) {
+          return {
+            replyText: res[0].output,
+            suggestions: []
+          };
+        }
+
+        return {
+          replyText: res?.data?.replyText,
+          suggestions: res?.data?.suggestions || []
+        };
+      })
+    );
+  }
+}
