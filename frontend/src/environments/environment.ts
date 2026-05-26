aca5886103d9b59b310862e4fa2e56cc6d4f1a60
Daniel Oliveira <161095611+DanielOv1103@users.noreply.github.com>
2026-05-26 00:24:45 -0500

    package json update

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiBaseUrl: 'http://localhost:5021/api',
-  advisorChatbotUrl: 'https://n8n-production-7f55.up.railway.app/webhook-test/ecoasis',
-  generalChatbotUrl: 'https://n8n-production-7f55.up.railway.app/webhook-test/ecoflow'
+  advisorChatbotUrl: 'https://n8n-production-7f55.up.railway.app/webhook/ecoasis',
+  generalChatbotUrl: 'https://n8n-production-7f55.up.railway.app/webhook/ecoflow'
 } as const;
